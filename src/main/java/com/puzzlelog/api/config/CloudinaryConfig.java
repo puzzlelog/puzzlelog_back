c11d998cb6b44e3a380f733b231a78569c46826b
jinwoo kim <critopia@naver.com>
2025-03-17 07:00:14 +0900

    config set

--- src/main/java/com/puzzlelog/api/config/CloudinaryConfig.java
+++ src/main/java/com/puzzlelog/api/config/CloudinaryConfig.java
@@ -1,20 +1,14 @@
 package com.puzzlelog.api.config;
 
-import com.cloudinary.Cloudinary;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.cloudinary.Cloudinary;
+
 @Configuration
 public class CloudinaryConfig {
-
     @Bean
     public Cloudinary cloudinary() {
-        String cloudinaryUrl = System.getenv("CLOUDINARY_URL");  // ✅ 환경 변수를 직접 가져오기
-
-        if (cloudinaryUrl == null || cloudinaryUrl.isEmpty()) {
-            throw new RuntimeException("❌ CLOUDINARY_URL 환경 변수가 설정되지 않았습니다.");
-        }
-
-        return new Cloudinary(cloudinaryUrl);
+        return new Cloudinary(System.getenv("CLOUDINARY_URL"));
     }
 }
