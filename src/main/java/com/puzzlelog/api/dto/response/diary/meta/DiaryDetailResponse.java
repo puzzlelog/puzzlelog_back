8b74c3981d6aea05aa1c8c13b8e0ab9a600b2c97
JinWoo Kim <critopia@naver.com>
2025-03-26 17:00:24 +0900

    Revert "Feature/diary"

--- src/main/java/com/puzzlelog/api/dto/response/diary/meta/DiaryDetailResponse.java
+++ src/main/java/com/puzzlelog/api/dto/response/diary/meta/DiaryDetailResponse.java
@@ -38,9 +38,6 @@ public class DiaryDetailResponse {
     private Instant updatedAt;
 
     private List<DiaryElementResponse> elements;
-    
-    private List<String> participants; //협업일기참가자
-
 
     public static DiaryDetailResponse from(
             Diary diary,
@@ -52,7 +49,6 @@ public class DiaryDetailResponse {
         return DiaryDetailResponse.builder()
             .diaryId(diary.getId())
             .userId(diary.getUserId())
-            .participants(diary.getParticipants())
             .title(diary.getTitle())
             .background(AssetResponse.from(background))
             .themeColor(diary.getThemeColor())
